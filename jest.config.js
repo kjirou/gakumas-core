2b9307f3234856bc783e243f9f30b0bb3b39b9b5
kjirou <kjirou.web@gmail.com>
2024-05-28 10:07:13 +0900

    Support ESM
    Ref: https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest/presets/default-esm",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };

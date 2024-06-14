81aadf76124a7812b9caba0f2fc900cb5e063f18
kjirou <kjirou.web@gmail.com>
2024-06-15 08:07:32 +0900

    Add a real data of producer items("Pアイテム")

---
+++ src/data/producer-item/index.test.ts
@@ -0,0 +1,64 @@
+import { producerItems } from "./index";
+
+test("any id is not duplicated", () => {
+  let ids: string[] = [];
+  for (const producerItem of producerItems) {
+    expect(ids).not.toContain(producerItem.id);
+    ids = [...ids, producerItem.id];
+  }
+});
+
+// 存在するデータのパターンから、法則を推測し、それに適合しているかでデータの検証を行う。
+// 今後、新しいデータの出現によって、法則が崩れてテストが成立しなくなる可能性はある。
+for (const producerItem of producerItems) {
+  describe(`${producerItem.name}(${producerItem.id})`, () => {
+    test("an id should be written in Hepburn romanization", () => {
+      const nonHepburnWords = [
+        "si",
+        "ti",
+        "tu",
+        "hu",
+        "zi",
+        "di",
+        "du",
+        "sya",
+        "syu",
+        "syo",
+        "tya",
+        "tyu",
+        "tyo",
+        "nb",
+        "nm",
+        "np",
+        "cch",
+      ];
+      for (const word of nonHepburnWords) {
+        expect(producerItem.id).not.toMatch(word);
+      }
+    });
+    test("idol's producer item should have an enhancement pattern", () => {
+      if (producerItem.producerItemProviderKind === "idol") {
+        expect(producerItem.enhanced).not.toBeUndefined();
+      }
+    });
+    test("a producer item obtained during the game cannot be enhanced", () => {
+      if (
+        producerItem.producerItemProviderKind === "supportCard" ||
+        producerItem.producerItemProviderKind === "others"
+      ) {
+        expect(producerItem.enhanced).toBeUndefined();
+      }
+    });
+    test("an enhanced producer item has a number of uses that is equal to or greater than its pre-enhanced value", () => {
+      if (
+        producerItem.base.times !== undefined &&
+        producerItem.enhanced &&
+        producerItem.enhanced.times !== undefined
+      ) {
+        expect(producerItem.enhanced.times).toBeGreaterThanOrEqual(
+          producerItem.base.times,
+        );
+      }
+    });
+  });
+}

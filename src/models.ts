1085e34ad41b4b496974bb7f88256b3d34d622f3
kjirou <kjirou.web@gmail.com>
2024-07-29 20:20:41 +0900

    プロデュースアイドル初期化時に初期カードセットを加えない
    シミュレーターとして使う際に、アイドル固定スキルカード以外は、編集ツール上で選択するはず。
    
    変わりに、追加するスキルカード・Pアイテムを別引数で受け取る。

--- src/models.ts
+++ src/models.ts
@@ -106,10 +106,8 @@ export const isRemainingProducerItemTimes = (
 
 /**
  * 初期スキルカードセットを生成する
- *
- * - TODO: 引数から強化済みスキルカードを指定できるようにする、才能開花4用
  */
-const createDefaultCardSet = (
+export const createDefaultCardSet = (
   producePlan: ProducePlan,
   idGenerator: IdGenerator,
 ): CardInProduction[] => {
@@ -127,12 +125,16 @@ const createDefaultCardSet = (
 /**
  * プロデュースアイドルを生成する
  *
- * @param param.deck 山札全体を明示的に指定し、キャラクター固有を生成しないなど本来の処理を通さない。テスト用。
- * @param param.producerItems Pアイテム全体を指定し、キャラクター固有を生成しないなど本来の処理を通さない。テスト用。
+ * @param param.additionalCards アイドル固有に加えて、追加するスキルカードリスト
+ * @param param.additionalProducerItems アイドル固有に加えて、追加するPアイテムリスト
+ * @param param.deck 山札全体を明示的に指定する。アイドル固有を生成しないなど本来の処理を通さない。テスト用。
+ * @param param.producerItems Pアイテム全体を指定する。アイドル固有を生成しないなど本来の処理を通さない。テスト用。
  * @param param.specialTrainingLevel 特訓段階
  * @param param.talentAwakeningLevel 才能開花段階
  */
 export const createIdolInProduction = (params: {
+  additionalCards?: CardInProduction[];
+  additionalProducerItems?: ProducerItemInProduction[];
   deck?: CardInProduction[];
   idGenerator: IdGenerator;
   idolDataId: IdolData["id"];
@@ -146,7 +148,6 @@ export const createIdolInProduction = (params: {
   const specificProducerItemData = getProducerItemDataById(
     idolData.specificProducerItemId,
   );
-  // TODO: 才能開花1段階目のランダムスキルカード強化
   const deck =
     params.deck ??
     [
@@ -155,7 +156,7 @@ export const createIdolInProduction = (params: {
         data: specificCardData,
         enhanced: params.specialTrainingLevel >= 3,
       },
-      ...createDefaultCardSet(idolData.producePlan, params.idGenerator),
+      ...(params.additionalCards ?? []),
     ].sort((a, b) => compareDeckOrder(a.data, b.data));
   const producerItems = params.producerItems ?? [
     {
@@ -163,6 +164,7 @@ export const createIdolInProduction = (params: {
       data: specificProducerItemData,
       enhanced: params.talentAwakeningLevel >= 2,
     },
+    ...(params.additionalProducerItems ?? []),
   ];
   return {
     deck,

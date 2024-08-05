6f62c7ccf7c4cebf60b5d2a18d6285f6a1c373ee
kjirou <kjirou.web@gmail.com>
2024-08-05 21:11:54 +0900

    微修正

--- src/data/cards.test.ts
+++ src/data/cards.test.ts
@@ -7,44 +7,43 @@ import {
   getCardDataById,
 } from "./cards";
 
-test("any id is not duplicated", () => {
+test("idが重複していない", () => {
   let ids: string[] = [];
   for (const card of cards) {
     expect(ids).not.toContain(card.id);
     ids = [...ids, card.id];
   }
 });
-
 // 存在するデータのパターンから、法則を推測し、それに適合しているかでデータの検証を行う。
 // 今後、新しいデータの出現によって、法則が崩れてテストが成立しなくなる可能性はある。
 for (const card of cards) {
-  test("an id should be written in Hepburn romanization", () => {
-    // id値は複数単語を区切りなく連結しているため、特に母音が後の単語の先頭になるときのパターンはリストに加えられない
-    // 例えば、"ou"は、「〜の/う〜」のような単語の場合、"no/u"が連続する
-    const nonHepburnWords = [
-      "si",
-      "ti",
-      "tu",
-      /(?<!s)hu/,
-      "zi",
-      "di",
-      "du",
-      "sya",
-      "syu",
-      "syo",
-      "tya",
-      "tyu",
-      "tyo",
-      "nb",
-      "nm",
-      "np",
-      "cch",
-    ];
-    for (const word of nonHepburnWords) {
-      expect(card.id).not.toMatch(word);
-    }
-  });
   describe(`${card.name}(${card.id})`, () => {
+    test("id文字列はヘボン式ローマ字表記である", () => {
+      // id値は複数単語を区切りなく連結しているため、特に母音が後の単語の先頭になるときのパターンはリストに加えられない
+      // 例えば、"ou"は、「〜の/う〜」のような単語の場合、"no/u"が連続する
+      const nonHepburnWords = [
+        "si",
+        "ti",
+        "tu",
+        /(?<!s)hu/,
+        "zi",
+        "di",
+        "du",
+        "sya",
+        "syu",
+        "syo",
+        "tya",
+        "tyu",
+        "tyo",
+        "nb",
+        "nm",
+        "np",
+        "cch",
+      ];
+      for (const word of nonHepburnWords) {
+        expect(card.id).not.toMatch(word);
+      }
+    });
     test("アイドル固有スキルカードは、必ず「重複不可」「レッスン中1回」を持つ", () => {
       if (card.cardProviderKind === "idol") {
         expect(card.nonDuplicative).toBe(true);
